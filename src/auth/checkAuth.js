3e36198ebbbc08894913d10ec55c215cd5525ab7
duc vinh <ducvinh100503@gmail.com>
2024-10-22 17:04:59 +0700

    check api

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+const { findById } = require("../services/apiKey.service");
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+
+    // check objKey
+    const objKey = await findById(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    req.objKey = objKey;
+    return next();
+  } catch (error) {}
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    if (!req.objKey.permission) {
+      return res.status(403).json({
+        message: "Permission dinied",
+      });
+    }
+
+    console.log("permission::", req.objKey.permission);
+    const validPermission = req.objKey.permission.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: "permission denied",
+      });
+    }
+  };
+};
+
+module.exports = {
+  apiKey,
+  permission,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const router = require("express").Router();
+const { apiKey, permission } = require("../auth/checkAuth");
+
+// check apiKey
+router.use(apiKey);
+
+// check permission
+router.use(permission("0000"));
 
 router.use("/v1/api", require("./access"));
 
